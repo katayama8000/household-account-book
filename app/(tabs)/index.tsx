0e06b28ee3a0de1bfd8ddf56a3c61090043ca639
katayama800 <tattu.0310@gmail.com>
2024-05-05 21:19:12 +0900

    wip supabase

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,6 @@
 import { StyleSheet } from 'react-native';
-
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
-import { Link } from 'expo-router';
 
 export default function TabOneScreen() {
   return (
