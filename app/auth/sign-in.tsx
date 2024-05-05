12b92f54ba1c27a8d9542b1f8e8be21e1ac969b9
katayama800 <tattu.0310@gmail.com>
2024-05-05 21:45:34 +0900

    signin

--- app/auth/sign-in.tsx
+++ app/auth/sign-in.tsx
@@ -1,12 +1,76 @@
 import { View, Text } from '@/components/Themed';
-import React from 'react';
+import { supabase } from '@/lib/supabase';
+import { useRouter } from 'expo-router';
+import React, { useState } from 'react';
+import { Alert, Button, TextInput, StyleSheet } from 'react-native';
 
 const SignInScreen = () => {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(false);
+  const { push } = useRouter();
+
+  const signInWithEmail = async () => {
+    setLoading(true);
+    const { error } = await supabase.auth.signInWithPassword({
+      email: email,
+      password: password,
+    });
+
+    if (error) {
+      Alert.alert(error.message);
+    } else {
+      Alert.alert('Signed in!');
+      push('/sample');
+    }
+    setLoading(false);
+  };
+
   return (
-    <View>
-      <Text>Sign Up Screen</Text>
+    <View style={styles.container}>
+      <View style={[styles.verticallySpaced, styles.mt20]}>
+        <TextInput
+          onChangeText={(text) => setEmail(text)}
+          value={email}
+          placeholder="email@address.com"
+          autoCapitalize={'none'}
+          style={{ color: 'white' }}
+        />
+      </View>
+      <View style={styles.verticallySpaced}>
+        <TextInput
+          onChangeText={(text) => setPassword(text)}
+          value={password}
+          secureTextEntry={true}
+          placeholder="Password"
+          autoCapitalize={'none'}
+          style={{ color: 'white' }}
+        />
+      </View>
+      <View style={[styles.verticallySpaced, styles.mt20]}>
+        <Button
+          title="Sign in"
+          disabled={loading}
+          onPress={() => signInWithEmail()}
+        />
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 40,
+    padding: 12,
+  },
+  verticallySpaced: {
+    paddingTop: 4,
+    paddingBottom: 4,
+    alignSelf: 'stretch',
+  },
+  mt20: {
+    marginTop: 20,
+  },
+});
 
 export default SignInScreen;
--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,8 +2,9 @@ import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = YOUR_REACT_NATIVE_SUPABASE_URL;
-const supabaseAnonKey = YOUR_REACT_NATIVE_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://nvldqfptrmwokpagptwh.supabase.co';
+const supabaseAnonKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im52bGRxZnB0cm13b2twYWdwdHdoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM2MjIwNDQsImV4cCI6MjAyOTE5ODA0NH0.Qe0umnN1qGN9_dSTFbLZG_O2pKTSQK1lguqUljoe9Ls';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
