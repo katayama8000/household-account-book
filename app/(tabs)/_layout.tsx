2bb801d5d07826345542581f73b382a25ee22091
katayama800 <tattu.0310@gmail.com>
2024-06-16 23:18:38 +0900

    show month

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,8 +1,10 @@
 import { TabBarIcon } from "@/components/navigation/TabBarIcon";
 import { Colors } from "@/constants/Colors";
+import dayjs from "dayjs";
 import { Tabs } from "expo-router";
 
 export default function TabLayout() {
+  const month = dayjs().month() + 1;
   return (
     <Tabs
       screenOptions={{
@@ -13,7 +15,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: "今月",
+          title: `${month}月`,
           tabBarIcon: ({ color, focused }) => <TabBarIcon name={focused ? "today" : "today-outline"} color={color} />,
         }}
       />
