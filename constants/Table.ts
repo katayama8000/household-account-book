ee7421b7f5d9219aefa0ef7e347e977db424578c
katayama800 <tattu.0310@gmail.com>
2024-06-23 16:06:38 +0900

    example

--- constants/Table.ts
+++ constants/Table.ts
@@ -2,4 +2,8 @@ const dev_couples = "dev_couples";
 const dev_monthly_invoices = "dev_monthly_invoices";
 const dev_payments = "dev_payments";
 
+// const dev_couples = "couples";
+// const dev_monthly_invoices = "monthly_invoices";
+// const dev_payments = "payments";
+
 export { dev_couples, dev_monthly_invoices, dev_payments };
