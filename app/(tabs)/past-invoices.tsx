5accb81b9a2916b6a7d6df8a9bbf4550686071a4
katayama800 <tattu.0310@gmail.com>
2024-06-24 20:43:58 +0900

    sort

--- app/(tabs)/past-invoices.tsx
+++ app/(tabs)/past-invoices.tsx
@@ -29,7 +29,13 @@ const PastInvoicesScreen = () => {
       }}
     >
       <FlatList
-        data={invoices}
+        // 月と年の降順で並び替え
+        data={invoices.sort((a, b) => {
+          if (a.year === b.year) {
+            return b.month - a.month;
+          }
+          return b.year - a.year;
+        })}
         renderItem={({ item }) => <MonthlyInvoice invoice={item} routerPush={push} />}
         keyExtractor={(item) => item.id.toString()}
         ItemSeparatorComponent={() => <View style={{ height: 4 }} />}
