aec73283c50b4d95487cfd80348276f42be66ee2
katayama800 <tattu.0310@gmail.com>
2024-06-25 08:57:03 +0900

    tables

--- constants/Table.ts
+++ constants/Table.ts
@@ -1,9 +1,11 @@
-const dev_couples = "dev_couples";
-const dev_monthly_invoices = "dev_monthly_invoices";
-const dev_payments = "dev_payments";
+let dev_couples = "dev_couples";
+let dev_monthly_invoices = "dev_monthly_invoices";
+let dev_payments = "dev_payments";
 
-// const dev_couples = "couples";
-// const dev_monthly_invoices = "monthly_invoices";
-// const dev_payments = "payments";
+if (process.env.EXPO_PUBLIC_EXPO_PUBLIC_APP_ENV === "production") {
+  dev_couples = "couples";
+  dev_monthly_invoices = "monthly_invoices";
+  dev_payments = "payments";
+}
 
 export { dev_couples, dev_monthly_invoices, dev_payments };
