c31d9b25e59b6a09a020b699720efa9b0bd4f518
katayama800 <tattu.0310@gmail.com>
2024-06-25 12:58:33 +0900

    add type

--- constants/Table.ts
+++ constants/Table.ts
@@ -1,6 +1,6 @@
-let dev_couples = "dev_couples";
-let dev_monthly_invoices = "dev_monthly_invoices";
-let dev_payments = "dev_payments";
+let dev_couples: "dev_couples" | "couples" = "dev_couples";
+let dev_monthly_invoices: "dev_monthly_invoices" | "monthly_invoices" = "dev_monthly_invoices";
+let dev_payments: "dev_payments" | "payments" = "dev_payments";
 
 if (process.env.EXPO_PUBLIC_EXPO_PUBLIC_APP_ENV === "production") {
   dev_couples = "couples";
