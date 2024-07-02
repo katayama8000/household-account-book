0b8d74e8acd6cf22cb2b68ad541e67d51e615d65
katayama800 <tattu.0310@gmail.com>
2024-07-02 23:11:41 +0900

    fix generate_types.sh

--- generate_types.sh
+++ generate_types.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
+source ./.env
+
 # Start generating TypeScript types for your Supabase schema
 echo "Generating TypeScript types for your Supabase schema..."
 
 # Ensure the PROJECT_REF environment variable is set
-if [ -z "$PROJECT_REF" ]; then
-  echo "Error: PROJECT_REF environment variable is not set." >&2
+if [ -z $PROJECT_REF ]; then
+  echo "PROJECT_REF environment variable is not set in .env file" >&2
   exit 1
 fi
 
 # Generate TypeScript types for your Supabase schema
 npx supabase gen types typescript \
-  --project-id "$PROJECT_REF" \
+  --project-id $PROJECT_REF \
   --schema public > types/supabase.ts
 
 # Check if the generation was successful
