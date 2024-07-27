f68991acb692d1a584d7cbee52d7f0b0927ea783
katayama8000 <63860688+katayama8000@users.noreply.github.com>
2024-07-27 17:18:46 +0900

    Google services json (#94)
    * delete
    
    * refactor
    
    * delete console
    
    * fix
    
    * moufu

--- app.config.ts
+++ app.config.ts
@@ -1,53 +1,46 @@
 import type { ConfigContext, ExpoConfig } from "expo/config";
 
 const allAppEnvs = ["production", "development", "local"] as const;
-
 type AppEnv = (typeof allAppEnvs)[number];
 
-const bundleIdByEnv = (appEnv: AppEnv): string => {
-  switch (appEnv) {
-    case "production":
-      return "com.katayama9000.householdaccountbook";
-    case "development":
-      return "com.katayama9000.householdaccountbook.dev";
-    case "local":
-      return "com.katayama9000.householdaccountbook.local";
-  }
+const envConfigs: Record<AppEnv, { bundleId: string; googleServicesJson: string; name: string }> = {
+  production: {
+    bundleId: "com.katayama9000.householdaccountbook",
+    googleServicesJson: "./google-services-prd.json",
+    name: "もうふといくら",
+  },
+  development: {
+    bundleId: "com.katayama9000.householdaccountbook.dev",
+    googleServicesJson: "./google-services-dev.json",
+    name: "(dev)もうふといくら",
+  },
+  local: {
+    bundleId: "com.katayama9000.householdaccountbook.dev",
+    googleServicesJson: "./google-services-dev.json",
+    name: "(local)もうふといくら",
+  },
 };
 
-const googleServicesJsonByEnv = (appEnv: AppEnv): string => {
-  switch (appEnv) {
-    case "production":
-      return "./google-services-prd.json";
-    case "development":
-      return "./google-services-dev.json";
-    case "local":
-      return "./google-services-dev.json";
-  }
-};
+const isAppEnv = (s: string): s is AppEnv => allAppEnvs.includes(s as AppEnv);
 
-const isAppEnv = (s: string): s is AppEnv => allAppEnvs.some((e) => e === s);
-
-const appEnv = process.env.APP_ENV ?? "local";
+const appEnv = (process.env.APP_ENV ?? "local") as AppEnv;
 
 if (!isAppEnv(appEnv)) throw new Error(`unsupported APP_ENV: ${appEnv}`);
 
 export default ({ config }: ConfigContext): ExpoConfig => {
-  console.log(`appEnv: ${appEnv}`);
-  console.log(`bundleId: ${bundleIdByEnv(appEnv)}`);
-  console.log(`googleServiceFile: ${googleServicesJsonByEnv(appEnv)}`);
+  const { bundleId, googleServicesJson, name } = envConfigs[appEnv];
   return {
     ...config,
     slug: "household-account-book",
-    name: `もうふといくら${appEnv === "production" ? "" : `(${appEnv})`}`,
+    name,
     extra: {
       ...config.extra,
     },
     android: {
       ...config.android,
-      package: bundleIdByEnv(appEnv),
+      package: bundleId,
       // FIXME: get GOOGLE_SERVICES_JSON from expo secrets
-      googleServicesFile: googleServicesJsonByEnv(appEnv),
+      googleServicesFile: googleServicesJson,
     },
   };
 };
