ab1e39dee7d7fa2e6bee7281c87b65b2b8c3f2d3
katayama8000 <63860688+katayama8000@users.noreply.github.com>
2025-01-03 12:42:06 +0900

    calu days in month (#191)

--- constants/RecurringPayments.ts
+++ constants/RecurringPayments.ts
@@ -1,5 +1,17 @@
-export const RecurringPayments = [
-  { amount: 2000, item: "ガソリン", memo: "自動追加" },
-  { amount: 3750, item: "通信費", memo: "自動追加" },
-  { amount: 30000, item: "家賃", memo: "自動追加" },
-] as const;
+import type { Dayjs } from "dayjs";
+
+export const RecurringPayments = (startOfNextMonth: Dayjs) => {
+  const daysInMonth = startOfNextMonth.daysInMonth();
+  const formattedDate = startOfNextMonth.format("YYYY-MM");
+  const recurringPayments = [
+    { amount: 2000, item: "ガソリン", memo: "自動追加" },
+    { amount: 3750, item: "通信費", memo: "自動追加" },
+    {
+      amount: 1000 * daysInMonth,
+      item: "家賃",
+      memo: `自動追加 ${formattedDate} ${daysInMonth}日間`,
+    },
+  ] as const;
+
+  return recurringPayments;
+};
